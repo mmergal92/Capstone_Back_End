e4f4a34e685a475f1a9ace02c8440d8aa4934618
mmergal92 <79428668+mmergal92@users.noreply.github.com>
2021-08-06 17:21:04 -0400

    added starter files

---
+++ server.js
@@ -0,0 +1,60 @@
+//////////////////////////////
+//DEPENDENCIES
+/////////////////////////////
+
+// Import Dependencies 
+
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const session = require('express-session');
+
+require('dotenv').config();
+
+// Create App Object
+
+const app = express();
+
+
+//Importing Models
+
+// const rssFeed = require('./models/rssFeed.js')
+
+// Middleware 
+
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+app.use(express.static('public')); // In 'public' folder we can put files to have access anywhere
+app.use(session({
+    secret: "supersecret",
+    resave: false,
+    saveUninitialized: false
+}))
+// Routes
+
+app.get("/", (req, res) =>{
+    res.send("Server Working!");
+});
+
+
+// Controllers
+
+// const apiStockDataController = require('./controllers/ApiStock.js');
+// app.use('/api', apiStockDataController);
+
+// Declaring Ports
+
+const PORT = process.env.PORT || 4000;
+
+// Database Connection
+
+// mongoose.connect(process.env.DATABASE_URL_TEST, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
+// mongoose.connection.once('open', () => {
+//     console.log('Linked to MongoDB')
+// })
+
+// Server Listener
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
