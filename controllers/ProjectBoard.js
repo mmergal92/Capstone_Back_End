5c048c0da20470baf3f1802a2538b8978a5b4bef
mmergal92 <79428668+mmergal92@users.noreply.github.com>
2021-08-13 17:09:15 -0400

    updated project crud

--- controllers/ProjectBoard.js
+++ controllers/ProjectBoard.js
@@ -63,6 +63,46 @@ router.post('/', (req, res) => {
     console.log('testing post')
 });
 
+// Find route
+router.get('/:id', async(req, res) =>{
+    await projectOverview.find({id: req.params.id}, (error, foundComments)=>{
+        console.log(req.params)
+        console.log(foundComments)
+        console.log(error)
+        res.send(foundComments)
+    }) 
+});
+
+// UPDATE route - EDIT
+router.get('/:id/edit', async(req, res) =>{
+    projectOverview.findById(req.params.id, (error, oneComment)=>{
+        console.log(req.params.id)
+        console.log(oneComment)
+        console.log(error)
+        res.send(oneComment)
+    }) 
+});
+
+// UPDATE route - PUT
+router.put('/:id', async(req, res) =>{
+    projectOverview.findByIdAndUpdate(req.params.id, req.body, {new: true}, (error, updatedComment)=>{
+        console.log(req.params.id + " and this is the req body: " + req.body)
+        console.log(updatedComment)
+        console.log(error)
+        res.send(updatedComment)
+    }) 
+});
+
+// UPDATE route - DELETE
+router.delete('/:id', async(req, res) =>{
+    await projectOverview.findOneAndRemove({_id: req.params.id}, (error, deletedComment)=>{
+        console.log({_id:req.params.id})
+        console.log(deletedComment)
+        console.log(error)
+        res.send(deletedComment)
+    }) 
+});
+
 
 // EXPORT
 module.exports = router;
